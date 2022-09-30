40bec9ac1b397a36d8a52a6d937125cb87c0e65f
dlalstmd <minid0329@gmail.com>
2022-09-30 15:50:24 +0900

    hellgi project restart

---
+++ app/src/main/java/com/example/projecthellgi/Login_page.java
@@ -0,0 +1,88 @@
+package com.example.projecthellgi;
+
+import static android.content.ContentValues.TAG;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class Login_page extends AppCompatActivity {
+
+    Button  mLoginBtn;
+    TextView mResigettxt;
+    EditText mEmailText, mPasswordText;
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_page);
+
+        ActionBar actionBar = getSupportActionBar();
+        assert actionBar != null;
+        actionBar.setTitle("로그인");
+
+        actionBar.setDisplayHomeAsUpEnabled(true); //뒤로가기버튼
+        actionBar.setDisplayShowHomeEnabled(true); //홈 아이콘
+
+        firebaseAuth =  FirebaseAuth.getInstance();
+        //버튼 등록하기
+        mResigettxt = findViewById(R.id.registerbtn);
+        mLoginBtn = findViewById(R.id.logInButton);
+        mEmailText = findViewById(R.id.idInput);
+        mPasswordText = findViewById(R.id.pwInput);
+
+        //가입 버튼이 눌리면
+        mResigettxt.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                //intent함수를 통해 register액티비티 함수를 호출한다.
+                startActivity(new Intent(Login_page.this,RegisterPage.class));
+            }
+        });
+
+        //로그인 버튼이 눌리면
+        mLoginBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                String email = mEmailText.getText().toString().trim();
+                String pwd = mPasswordText.getText().toString().trim();
+                firebaseAuth.signInWithEmailAndPassword(email,pwd)
+                        .addOnCompleteListener(Login_page.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()){
+                                    Log.d(TAG, "signInWithEmail:success");
+
+                                    Intent intent = new Intent(Login_page.this, HomeActivity.class);
+                                    startActivity(intent);
+                                }else{
+                                    Log.w(TAG, "signInWithEmail:failure", task.getException());
+                                    Toast.makeText(Login_page.this,"로그인 오류",Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
+    }
+    public boolean onSupportNavigateUp(){
+        onBackPressed();; // 뒤로가기 버튼이 눌렸을시
+        return super.onSupportNavigateUp(); // 뒤로가기 버튼
+    }
+}
---
+++ app/src/main/java/com/example/projecthellgi/RegisterPage.java
@@ -0,0 +1,87 @@
+package com.example.projecthellgi;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+
+public class RegisterPage extends AppCompatActivity {
+
+    private static final String TAG = "RegisterPage";
+    private FirebaseAuth mAuth;
+    private Button sign_up;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_page);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        sign_up = findViewById(R.id.registerCompliteButton);
+        sign_up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signUp();
+            }
+        });
+    }
+
+    // When initializing your Activity, check to see if the user is currently signed in.
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+
+    private void signUp() {
+        // 이메일
+        EditText emailEditText = findViewById(R.id.UserEmailAddress);
+        String email = emailEditText.getText().toString();
+        // 비밀번호
+        EditText passwordEditText = findViewById(R.id.UserPassword);
+        String password = passwordEditText.getText().toString();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            Intent intent = new Intent(RegisterPage.this, MainActivity.class);
+                            startActivity(intent);
+                            Toast.makeText(RegisterPage.this, "등록 완료", Toast.LENGTH_SHORT).show();
+                            finish();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(RegisterPage.this, "등록 에러", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                    }
+                });
+    }
+
+    Intent intent = getIntent();
+
+}
